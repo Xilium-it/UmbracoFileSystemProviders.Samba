76d00be9d03b3bf2905965de87dbcc1e7cd3e11f
Flavio Spezi <fspezi@xilium.it>
2020-01-10 23:47:37 +0100

    Update DLL di XiliumLib

--- Resources/XL.UpdateDLL - Release - Xilium.ImageFlower.cmd
+++
@@ -1,9 +0,0 @@
-SET curr=.\
-
-cmd /c C:\CommandScripts\Scripts\ASP.NET_UpdateProjectDll.cmd "Xilium.ImageFlower\source" "Xilium.ImageFlower" "release" "%curr%"
-
-del "Xilium.ImageFlower - DEBUG.txt"
-del "Xilium.ImageFlower - RELEASE.txt"
-@ECHO Xilium.ImageFlower: RELEASE version - %date% %time% >"Xilium.ImageFlower - RELEASE.txt"
-
-SET curr=
